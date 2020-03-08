217d1f9391d662364728899126bb334498bb6e06
lin <2509415929@qq.com>
2020-03-08 15:46:35 +0800

    购物app搭建

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <keep-alive exclude="Detail"><router-view></router-view></keep-alive>
+    
+    <main-tab-bar/>
+    <icon></icon>
+    <svg-icon></svg-icon>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import MainTabBar from 'components/content/mainTabbar/MainTabBar'
+import Icon from 'components/content/Icon/Icon.vue'
+import SvgIcon from 'components/content/Icon/svg.vue'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() { 
+    return {
+
+    }
+  },
+  components:{
+    MainTabBar,
+    Icon,
+    SvgIcon,
   }
-}
+ }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+@import "./assets/css/base.css";
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,30 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+
+import FastClick from 'fastclick'
+import VueLazyLoad from 'vue-lazyload'
+
+import toast from 'components/common/toast'
 
 Vue.config.productionTip = false
 
+//解决移动端300ms延迟
+FastClick.attach(document.body)
+
+//安装toast插件
+Vue.use(toast)
+
+//安装懒加载插件
+Vue.use(VueLazyLoad, {
+  loading: require('./assets/img/common/placeholder.png')
+})
+
+Vue.prototype.$bus = new Vue()
+
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
